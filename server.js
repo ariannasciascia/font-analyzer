4b7764a20f4d75c687a9890510d045574e5e4f7c
ariannasciascia <arianna.sciascia@reddoak.com>
2025-07-25 16:44:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 const cors = require('cors');
 const path = require('path');
 
@@ -14,7 +14,8 @@ app.post('/analyze', async (req, res) => {
   try {
     const browser = await puppeteer.launch({
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      executablePath: '/usr/bin/google-chrome' // questo fa usare Chrome già presente su Render
     });
 
     const page = await browser.newPage();
